8b35290b696019ecf3f941d2235e6388bb093301
LiTatyana <lee.tanyusha123@gmail.com>
2022-12-01 21:09:27 +0500

    fix: complete card item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import { CardItem } from "./components/CardItem/CardItem";
 
+import { CardItem } from "./components/CardItem/CardItem";
+import React from 'react'
 function App() {
   return (
     <CardItem/>
   );
-=======
-import { Header } from "./components/Header/Header";
 
-import React from 'react'
-
-export const App = () => {
-  return (
-    <Header/>
-  )
->>>>>>> f5839953fdd9c265fa21778d9b4e015529aa1b64
 }
 
 export default App;
