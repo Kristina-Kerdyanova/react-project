8d0d999a0dd9722fd0ad5a195f187290d8289e7a
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-01 16:04:10 +0500

    feat: add main template

---
+++ src/components/MainTemplate/MainTemplate.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../Header/Header'
+import { StyledMainTemplate } from './style'
+
+export const MainTemplate = () => {
+  return (
+    <StyledMainTemplate>
+        <Header />
+        <Outlet />
+        {/* <Footer /> */}
+</StyledMainTemplate>
+  )
+}
---
+++ src/components/MainTemplate/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledMainTemplate = styled.div`
+  display: flex;
+  padding: 0px 16px;
+  min-width: 1440px;
+  width: 100%;
+  margin: 0px auto;
+`;
+
+export { StyledMainTemplate };
