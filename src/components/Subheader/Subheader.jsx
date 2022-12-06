2321bf96965106adf4d518ed7e743ae81fdd3960
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-06 12:38:17 +0500

    faet: change subheader

--- src/components/Subheader/Subheader.jsx
+++ src/components/Subheader/Subheader.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import User from "../../assets/icons/user.svg";
 import ArrowToDown from "../../assets/icons/arrow_to_down.svg";
-import Map from "../../assets/icons/map.svg";
 import { StyledSubheader, StyledSubheaderBlock, StyledSubheaderContainer, StyledSubheaderElement, StyledSubheaderElementContainer, StyledSubheaderText, StyledSubheaderTextBold } from './style'
+import { SubheaderAuthorization } from '../SubheaderAuthorization/SubheaderAuthorization';
+import { SubheaderMap } from '../SubheaderMap/SubheaderMap';
 
 export const Subheader = () => {
   return (
     <StyledSubheader>
       <StyledSubheaderContainer>
-        <StyledSubheaderElement>
-          <img src={User} alt="user" />
-          <StyledSubheaderTextBold>Вход / Регистрация</StyledSubheaderTextBold>
-          <img src={ArrowToDown} alt="arrow to down" />
-        </StyledSubheaderElement>
-        <StyledSubheaderElement>
-          <img src={Map} alt="map" />
-          <StyledSubheaderTextBold>Ваш город</StyledSubheaderTextBold>
-          <img src={ArrowToDown} alt="arrow to down" />
-        </StyledSubheaderElement>
+        <SubheaderAuthorization/>
+        <SubheaderMap/>
         <StyledSubheaderBlock>
           <StyledSubheaderElement>
             <StyledSubheaderElementContainer>
---
+++ src/components/SubheaderMap/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledFormMap = styled.form``;
+
+const StyledSelectMap = styled.select``;
+
+const StyledOptionMap = styled.option``;
+
+export { StyledFormMap, StyledOptionMap, StyledSelectMap };
