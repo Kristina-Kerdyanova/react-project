2577d97d22ea4e7742f83707f0e8b6e2b488978f
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-09 15:59:24 +0500

    feat: add main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BodyNavSelect } from '../components/BodyNavSelect/BodyNavSelect'
+import { Catalog } from '../components/Catalog/Catalog'
+import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
+import { News } from '../components/News/News'
+import { Subscribe } from '../components/Subscribe/Subscribe'
 
 export const Main = () => {
   return (
-   <Header/>
+   <>
+   <Header />
+   <BodyNavSelect/>
+   <Catalog/>
+   <News/>
+   <Subscribe/>
+   <Footer/>
+   </>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { News } from '../components/News/News'
-import { Catalog } from '../components/Catalog/Catalog'
-import { BodyNavSelect } from '../components/BodyNavSelect/BodyNavSelect'
+import { Main } from '../pages/Main'
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<BodyNavSelect/>} />
-      
+      <Route path="/" element={<Main/>} />
     </Routes>
   )
 }
