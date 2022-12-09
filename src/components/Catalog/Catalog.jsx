1e3ea33e658fb04b0acaedf1bd6b886be896dd7a
LiTatyana <lee.tanyusha123@gmail.com>
2022-12-09 11:28:18 +0500

    feat: Catalog component created

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import wds from "../../assets/images/wds.svg";
+import rehau from "../../assets/images/rehau.svg";
+import veka from "../../assets/images/veka.svg";
+import { StyledBodyNavBorder, StyledBodyNavEllements, StyledBodyNavHead, StyledBodyNavHover, StyledBodyNavSell, StyledBodyNavText, StyledBodyNavWrapper } from '../BodyNavSelect/style'
+import { StyledCatalog, StyledCatalogCard, StyledCatalogWrapper } from './style';
+
+export const Catalog = () =>{
+
+    return(
+        // <CatalogContainer>
+        //     <CatalogWrapper>
+        //         <CatalogCard><img src={wds} alt="wds" /></CatalogCard>
+        //         <CatalogCard><img src={rehau} alt="" /></CatalogCard>
+        //         <CatalogCard><img src={veka} alt="" /></CatalogCard>
+        //     </CatalogWrapper>
+        // </CatalogContainer>
+        <StyledCatalog>
+
+<StyledBodyNavWrapper>
+            <StyledBodyNavText>
+            <StyledBodyNavHead>
+            Каталог брендов
+            </StyledBodyNavHead>
+            <StyledBodyNavEllements>
+                <StyledBodyNavSell>Диваны </StyledBodyNavSell>
+                <StyledBodyNavSell>Кровати</StyledBodyNavSell>
+                <StyledBodyNavSell>Двери</StyledBodyNavSell>
+                <StyledBodyNavSell>Окна</StyledBodyNavSell>
+                <StyledBodyNavSell>Натяжные потолки</StyledBodyNavSell>
+            </StyledBodyNavEllements>
+            </StyledBodyNavText>
+            <StyledBodyNavBorder></StyledBodyNavBorder>
+
+
+        </StyledBodyNavWrapper>
+
+            <StyledCatalogWrapper>
+                <StyledCatalogCard src={wds} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={rehau} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={veka} alt=""></StyledCatalogCard>
+            </StyledCatalogWrapper>
+            <StyledCatalogWrapper>
+                <StyledCatalogCard src={wds} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={rehau} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={veka} alt=""></StyledCatalogCard>
+            </StyledCatalogWrapper>
+            <StyledCatalogWrapper>
+                <StyledCatalogCard src={wds} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={rehau} alt=""></StyledCatalogCard>
+                <StyledCatalogCard src={veka} alt=""></StyledCatalogCard>
+            </StyledCatalogWrapper>
+        </StyledCatalog>
+    );
+};
---
+++ src/components/Catalog/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const StyledCatalog = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 70px;
+
+`
+const StyledCatalogWrapper = styled.div`
+width: 100%;
+display: flex;
+gap: 30px;
+justify-content: center;
+align-items: center;
+`
+
+const StyledCatalogCard = styled.img`
+background: #FFFFFF;
+box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.18);
+border-radius: 37px;
+transition:.7s;
+width: 360px;
+height: 309px;
+&:hover{
+    filter: drop-shadow(0px 4px 28px rgba(119, 33, 193, 0.62));
+}
+`
+
+export{
+    StyledCatalog,
+    StyledCatalogCard,
+    StyledCatalogWrapper,
+}
