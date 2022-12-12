04249eb301ee87e3277a8a2a551a4ed595af953a
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-12 16:31:33 +0500

    delete: delete subheader map

--- src/components/SubheaderMap/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledFormMap = styled.form``;
-
-const StyledSelectMap = styled.select``;
-
-const StyledOptionMap = styled.option``;
-
-export { StyledFormMap, StyledOptionMap, StyledSelectMap };
