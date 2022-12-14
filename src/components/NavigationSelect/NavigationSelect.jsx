0767e8d5e76dfd2e9133fc949f4313e53e5d711c
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-14 17:22:08 +0500

    refactor: navigation select

--- src/components/NavigationSelect/NavigationSelect.jsx
+++ src/components/NavigationSelect/NavigationSelect.jsx
@@ -43,11 +43,11 @@ export const NavigationSelect = () => {
   };
   return (
     <StyledSelect>
-    <Select options={options}
-      styles={selectStyles}
-      defaultValue={options[0]}
-      isMulti={false}
-    />
+      <Select options={options}
+        styles={selectStyles}
+        defaultValue={options[0]}
+        isMulti={false}
+      />
     </StyledSelect>
   );
 }
@@ -56,38 +56,4 @@ const StyledSelect = styled.div`
   border-radius: 35px;
   border: solid 0px;
   box-shadow: (0px 4px 26px rgba(0, 0, 0, 0.18));
-`
-// export class NavigationSelect extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { value: 'all' };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//     this.setState({ value: event.target.value });
-//   }
-
-//   handleSubmit(event) {
-//     event.preventDefault();
-//   }
-
-//   render() {
-//     return (
-//       <StyledFormNavigation onSubmit={this.handleSubmit}>
-//         <StyledSelectNavigation value={this.state.value} onChange={this.handleChange}>
-//           <StyledOptionNavigation value="all">Все категории</StyledOptionNavigation>
-//           <StyledOptionNavigation value="doors">Двери</StyledOptionNavigation>
-//           <StyledOptionNavigation value="windows">Окна</StyledOptionNavigation>
-//           <StyledOptionNavigation value="ceilings">Натяжные потолки</StyledOptionNavigation>
-//           <StyledOptionNavigation value="sofas">Диваны/кровати</StyledOptionNavigation>
-//           <StyledOptionNavigation value="blinds">Жалюзи/ролеты</StyledOptionNavigation>
-//           <StyledOptionNavigation value="furniture">Мебель</StyledOptionNavigation>
-//           <StyledOptionNavigation value="forged_products">Кованые изделия</StyledOptionNavigation>
-//         </StyledSelectNavigation>
-//       </StyledFormNavigation>
-//     );
-//   }
-// }
+`;
