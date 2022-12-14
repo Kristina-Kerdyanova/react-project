b3fb170a6105a2eff21232a347c362cb7844de0e
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-14 17:27:17 +0500

    update: main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BodyNavSelect } from '../components/BodyNavSelect/BodyNavSelect'
 import { Catalog } from '../components/Catalog/Catalog'
+import { DetailsItem } from '../components/DetailsItem/DetailsItem'
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
 import { News } from '../components/News/News'
@@ -10,6 +11,7 @@ export const Main = () => {
   return (
    <>
    <Header />
+   <DetailsItem/>
    <BodyNavSelect/>
    <Catalog/>
    <News/>
