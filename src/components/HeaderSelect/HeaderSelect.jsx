79c745d6d91a437803186cb553e635812c78f81e
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-14 17:23:36 +0500

    refactor: header select

--- src/components/HeaderSelect/HeaderSelect.jsx
+++ src/components/HeaderSelect/HeaderSelect.jsx
@@ -1,48 +1,99 @@
 import React from "react"
-import { OptionHeaderSelect, StyledFormHeaderSelect, StyledHeaderSelect } from "./style";
+import Select from 'react-select';
+import styled from 'styled-components';
 
-const content = [
-  {
-    language: [
-      { value: "Russian", option: "Русский" },
-      { value: "Uzbek", option: "Узбекский" },
-    ]
-  },
-  {
-    currency: [
-      { value: "Russian", option: "Русский" },
-      { value: "Uzbek", option: "Узбекский" },
-    ]
-  }
+
+const options = [
+  { value: "Russian", label: "Русский" },
+  { value: "Uzbek", label: "Узбекский" },
 ];
 
-export class HeaderSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
 
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+export const HeaderSelect = () => {
+  const selectStyles = {
+    control: (provided) => ({
+      ...provided,
+      border: "none",
+      textAlign: "center",
+      background: "none",
+    }),
 
-  render() {
-    return (
-      <StyledFormHeaderSelect onSubmit={this.handleSubmit}>
-        <StyledHeaderSelect value={this.state.value} onChange={this.handleChange}>
-          {content.map((item) => (
-            <OptionHeaderSelect value={content.value}>{item.option}</OptionHeaderSelect>
-          ))}
-        </StyledHeaderSelect>
-      </StyledFormHeaderSelect>
-    );
-  }
+    singleValue: (provided) => ({
+      ...provided,
+      color: "#7721c1",
+      fontWeight: "400",
+      fontSize: "13px",
+      lineHeight: "15px",
+      fontFamily: "Roboto",
+      appearance: "none",
+    }),
+
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: "none",
+    }),
+  };
+
+  return (
+    <StyledHeaderSelect>
+      <Select options={options}
+        styles={selectStyles}
+        defaultValue={options[0]}
+        isMulti={false}
+      />
+    </StyledHeaderSelect>
+  )
 }
+
+const StyledHeaderSelect = styled.div`
+  border: solid 0px;
+`;
+
+
+
+// const content = [
+//   {
+//     language: [
+//       { value: "Russian", option: "Русский" },
+//       { value: "Uzbek", option: "Узбекский" },
+//     ]
+//   },
+//   {
+//     currency: [
+//       { value: "uzs", option: "СУМ/UZS" },
+//       { value: "", option: "" },
+//     ]
+//   }
+// ];
+
+// export class HeaderSelect extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: "" };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <StyledFormHeaderSelect onSubmit={this.handleSubmit}>
+//         <StyledHeaderSelect value={this.state.value} onChange={this.handleChange}>
+//           {content.map((item) => (
+//             <OptionHeaderSelect value={item.value}>{item.option}</OptionHeaderSelect>
+//           ))}
+//         </StyledHeaderSelect>
+//       </StyledFormHeaderSelect>
+//     );
+//   }
+// }
 
 
--- src/components/HeaderSelect/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledFormHeaderSelect = styled.form``;
-
-const StyledHeaderSelect = styled.select``;
-
-const OptionHeaderSelect = styled.option``;
-
-export { StyledFormHeaderSelect, StyledHeaderSelect, OptionHeaderSelect };
