81ba811a6b5a71a54975e3014f88bb1d5dcd865a
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-16 19:50:15 +0500

    feat: add styles to label

---
+++ src/components/Label/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledLabel = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 107.5px;
+  min-height: 40px;
+  color: #ffffff;
+  font-family: "Roboto";
+  font-weight: 700;
+  font-size: 22px;
+  border-radius: 19px;
+  box-shadow: inset 0px 4px 33px rgba(0, 0, 0, 0.28);
+  margin-bottom: 12px;
+  &:first-child {
+    margin-right: 17.5px;
+  }
+`;
