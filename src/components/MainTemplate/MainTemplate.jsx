0e8c101fda4a39964b84c247a6d4424a3d2d591a
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-16 19:51:07 +0500

    feat: update main template

--- src/components/MainTemplate/MainTemplate.jsx
+++ src/components/MainTemplate/MainTemplate.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
 import { StyledMainTemplate } from './style'
 
 export const MainTemplate = () => {
   return (
     <StyledMainTemplate>
-      <Header />
-      <Outlet />
-      {/* <Footer /> */}
+      <Container>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Container>
     </StyledMainTemplate>
   )
 }
+
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: auto;
+`;
