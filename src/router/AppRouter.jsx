21a27c48ac8b5847ae395cbe85441efe66e18fbd
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-22 15:58:40 +0500

    feat: add cart, categories

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,20 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import { Main } from '../pages/Main'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
-import { MainTemplate } from '../components/MainTemplate/MainTemplate'
+import { Cart } from '../pages/Cart/Cart'
+import { Categories } from '../pages/Categories'
 
 export const AppRouter = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<MainTemplate />}> */}
       <Route index element={<Main />} />
       <Route path="/" element={<Main />} />
-      <Route path="/sign-in" element={<Login />} />
-      <Route path="/sign-up" element={<Register />} />
-      {/* <Route path="/account" element={<Account/>} />
-      <Route path="favorite" element={<Favorite/>} />
-      <Route path="search" element={<Search/>} /> */}
-      {/* </Route> */}
+      <Route path="sign-in" element={<Login />} />
+      <Route path="sign-up" element={<Register />} />
+      <Route path="cart" element={<Cart/>} />
+      <Route path="categories" element={<Categories/>} />
+      {/* <Route path="search" element={<Search/>} /> */}
     </Routes>
   )
 }
