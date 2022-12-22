66346a747feae5bb6e533ff1e2d3c5f28cb5f63d
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-22 16:03:05 +0500

    refactor: change structure main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,6 +4,7 @@ import { Catalog } from '../components/Catalog/Catalog'
 import { DetailsItem } from '../components/DetailsItem/DetailsItem'
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
+import { NavigationBar } from '../components/NavigationBar/NavigationBar'
 import { News } from '../components/News/News'
 import { Subscribe } from '../components/Subscribe/Subscribe'
 
@@ -11,6 +12,7 @@ export const Main = () => {
   return (
    <>
    <Header />
+   <NavigationBar/>
    <DetailsItem/>
    <BodyNavSelect/>
    <Catalog/>
