ca071384844b59e01ddf0a7f2ec05ceb80f60f8a
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-12-22 16:04:39 +0500

    feat: add input range

---
+++ src/components/InputRangePrice/InputRangePrice.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import InputRange from 'react-input-range';
+ 
+
+class InputRangePrice extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      value: { min: 2, max: 10 },
+    };
+  }
+ 
+  render() {
+    return (
+      <InputRange
+        maxValue={20}
+        minValue={0}
+        value={this.state.value}
+        onChange={value => this.setState({ value })} />
+    );
+  }
+}
+ 
+ReactDOM.render(
+  <InputRangePrice />,
+  document.getElementById('app')
+);
